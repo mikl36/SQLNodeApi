1b76c710ad3756a9543cfc97c73c18af265d64ee
Mika <156081546+mikl36@users.noreply.github.com>
2025-03-27 15:17:17 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => { // default answer to root
   res.status(httpStatusCode.HTTP_OK).json({ message: 'Connection OK' })
 })
 
-// user routes get all users, queries, update, delete
+// user routes get all users, queries, create, update, delete
 router.get('/v1/user', controllers.getAllUsers)
 router.get('/v1/user/query', controllers.getUserByQuery)
 router.post('/v1/user', controllers.createUser)
